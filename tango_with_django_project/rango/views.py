5987f26277c42bb2f96f1fa9e52be1240ef8a303
DragonCatExcelsior <degiojake@gmail.com>
2015-02-26 18:42:05 +0000

    Chapter 13 - completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -198,7 +198,7 @@ def index(request):
     if last_visit:
         last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
 
-        if (datetime.now() - last_visit_time).seconds > 0:
+        if (datetime.now() - last_visit_time).days > 0:
             # ...reassign the value of the cookie to +1 of what it was before...
             visits = visits + 1
             # ...and update the last visit cookie, too.
